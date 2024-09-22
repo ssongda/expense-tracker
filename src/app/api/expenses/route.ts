19c5a66f7e87ad15f7be866b641893f8baa1aa6c
TonakaCoder <hiro_tonaka@tonakacoder.com>
2024-09-23 08:41:23 +0900

    .

--- src/app/api/expenses/route.ts
+++ src/app/api/expenses/route.ts
@@ -1,8 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { PrismaClient } from '@prisma/client';
 import { unformatNumber } from '@/utils/common/formatNumber';
-
-const prisma = new PrismaClient();
+import { prisma } from '@/lib/prisma';
 
 export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams;
