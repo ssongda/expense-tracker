971f66f64b4a0eefcf7b94dc3ebb4a34a0aafacf
TonakaCoder <hiro_tonaka@tonakacoder.com>
2024-09-23 13:26:43 +0900

    fix responsive

--- src/components/GlobalHeader/index.tsx
+++ src/components/GlobalHeader/index.tsx
@@ -1,15 +1,29 @@
-import Link from "next/link";
-import styles from "./index.module.css";
+import Link from 'next/link';
+import { DollarSign } from 'react-feather';
+import styles from './index.module.css';
 
 export default function Header() {
   return (
     <header className={styles.header}>
       <div className={styles.logo}>
-        <Link href="/">지출 흐름 가계부</Link>
+        <Link
+          href="/"
+          className={styles.logoText}
+        >
+          지출 흐름 가계부
+        </Link>
+        <Link
+          href="/"
+          className={styles.logoIcon}
+        >
+          <DollarSign />
+        </Link>
       </div>
       <nav className={styles.nav}>
         <Link href="/expenses">지출 기록</Link>
-        <Link href="/analysis">소비 패턴 분석</Link>
+        <Link href="/analysis">
+          소비 패턴 분석
+        </Link>
       </nav>
     </header>
   );
